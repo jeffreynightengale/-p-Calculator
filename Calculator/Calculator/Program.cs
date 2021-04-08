b758f8f3fc11711fd8b402f05c7592dc26a66380
Jeff <Jeff@DESKTOP-DFBAMBV>
2021-04-08 18:47:28 -0500

    hello

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -7,40 +7,99 @@ namespace Calculator
         static void Main(string[] args)
         {
             DeveloperInformation("", "", "");
+
+            string final;
             
-            Console.WriteLine($"What type of calculation would you like to use? (Addition, Subtratcion, Multiplication, or Division) >>");
-            string calculate = Console.ReadLine();
+                Console.WriteLine($"What type of calculation would you like to use? (Addition, Subtratcion, Multiplication, or Division) >>");
+                string calculate = Console.ReadLine();
 
-            Console.WriteLine($"What is your first number? >>");
-            string firstNumber = Console.ReadLine();
-            double v1 = Convert.ToDouble(firstNumber);
+                Console.WriteLine($"What is your first number? >>");
+                string firstNumber = Console.ReadLine();
+                double v1 = Convert.ToDouble(firstNumber);
 
-            Console.WriteLine($"What is your second number? >>");
-            string secondNumber = Console.ReadLine();
-            double v2 = Convert.ToDouble(secondNumber);
+                Console.WriteLine($"What is your second number? >>");
+                string secondNumber = Console.ReadLine();
+                double v2 = Convert.ToDouble(secondNumber);
 
-            if (calculate.ToLower() == "addition")
-            {
-                 Add(v1, v2);
-            }
-            else if (calculate.ToLower() == "subtraction")
-            {
-                
-            }
-            else if (calculate.ToLower() == "multiplication")
-            {
+                double answer = 0;
+                if (calculate.ToLower() == "addition")
+                {
+                    answer = Add(v1, v2);
 
-            }
-            else if (calculate.ToLower() == "division")
-            {
+                    Console.WriteLine($"Your answer is {answer}");
+                }
+                else if (calculate.ToLower() == "subtraction")
+                {
+                    answer = Subtract(v1, v2);
 
-            }
-            else
+                    Console.WriteLine($"Your answer is {answer}");
+                }
+                else if (calculate.ToLower() == "multiplication")
+                {
+                    answer = Mutliply(v1, v2);
+
+                    Console.WriteLine($"Your answer is {answer}");
+                }
+                else if (calculate.ToLower() == "division")
+                {
+                    answer = Divide(v1, v2);
+
+                    Console.WriteLine($"Your answer is {answer}");
+                }
+                else
+                {
+                    Console.WriteLine("Please insert Addition, Subtraction, Multiplication, or Division.");
+                }
+
+            do
             {
-                Console.WriteLine("Please insert Addition, Subtraction, Multiplication, or Division.");
-            }
+                Console.WriteLine($"Do you want to do another calculation");
+                final = Console.ReadLine();
+                if (final.ToLower()[0] == 'n')
+                {
+                    Console.WriteLine("Thank you for using our service");
+                    Environment.Exit(-1);
+                }
+
+                Console.WriteLine($"Would you like to use your previous answer as your first value?");
+                string useFirstValue = Console.ReadLine();
+                if (useFirstValue.ToLower()[0] == 'y')
+                {
+                    Console.WriteLine($"What type of calculation would you like to use? (Addition, Subtratcion, Multiplication, or Division) >>");
+                    calculate = Console.ReadLine();
+                    v1 = answer;
+
+                    Console.WriteLine($"What is your second number? >>");
+                    secondNumber = Console.ReadLine();
+                    v2 = Convert.ToDouble(secondNumber);
+
+                    Console.WriteLine($"Your new answer is {answer}");
+                }
+                else
+                {
+                    Console.WriteLine($"What type of calculation would you like to use? (Addition, Subtratcion, Multiplication, or Division) >>");
+                    calculate = Console.ReadLine();
+
+                    Console.WriteLine($"What is your first number? >>");
+                    firstNumber = Console.ReadLine();
+                    v1 = Convert.ToDouble(firstNumber);
+
+                    Console.WriteLine($"What is your second number? >>");
+                    secondNumber = Console.ReadLine();
+                    v2 = Convert.ToDouble(secondNumber);
+
+                    Console.WriteLine($"Your new answer is {answer}");
+                }
+            while (final.ToLower()[0] == 'y');
+            } while (true);
+
         }
-
+        /// <summary>
+        /// This is used when the user wants to do Addition
+        /// </summary>
+        /// <param name="v1">First variable they input</param>
+        /// <param name="v2">Second variable they input</param>
+        /// <returns>Returns the sum of the two numbers (5+5)</returns>
         static double Add(double v1, double v2) //always declare variables in parenthesis
         {
             double sum = v1 + v2;
@@ -48,7 +107,12 @@ namespace Calculator
 
             //return val1 + val2;
         }
-
+        /// <summary>
+        /// This is used when the user wants to do addition
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
         static double Subtract(double v1, double v2)
         {
             double sum = v1 - v2;
@@ -72,6 +136,9 @@ namespace Calculator
             developerName = "Jeffrey Nightengale";
             className = "MIS 3013";
             dateOfWriting = "April 1, 2021";
+
+            Console.WriteLine($"{developerName} {className} {dateOfWriting}");
+            Console.WriteLine();
         }
     }
 }
